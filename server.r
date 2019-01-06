d4fb570caf1d10060f4cddf68110f95759eb454a
SCloves <clovesgtx@gmail.com>
2019-01-06 15:29:39 -0200

    aula 5

--- server.r
+++ server.r
@@ -1,8 +1,20 @@
 library(shiny)
 library(shinydashboard)
 
+setwd('/home/cloves/Documents/shiny/')
+
 shinyServer(function(input, output){
+  
   output$histogram <- renderPlot({
     hist(faithful$eruptions, breaks = input$bins)
+     })
+  
+  output$msgOutput <- renderMenu({
+    msgs <- apply(read.csv('messages.csv'), 1, function(row){
+      messageItem(from = row[['from']], message = row[['message']])
+    })
+    
+    dropdownMenu(type = "messages", .list = msgs)
+    
   })
 })
--- ui.r
+++ ui.r
@@ -2,24 +2,20 @@ library(shiny)
 library(shinydashboard)
 
 shinyUI(
-  dashboardPage( 
-    dashboardHeader(title = 'EstatCAPS',
-                    dropdownMenu(type = 'message',
-                                 messageItem(from = "Finance update",
-                                             message = "we are on threshold"),
-                                 messageItem(from = "Sales Update", message = "Sales are at 55%",
-                                             icon = icon("bar-chart"), time = "22:00"),
-                                 messageItem(from = "Sales update", message = "Sales meeting at 6 PM",
-                                             icon = icon("handshake-o"), time = "14:00")
-                      
-                    )
+  dashboardPage( dashboardHeader(title = 'EstatCAPS', dropdownMenuOutput('msgOutput')
+#                    dropdownMenu(type = 'message',
+#                                 messageItem(from = "Finance update",
+#                                             message = "we are on threshold"),
+#                                 messageItem(from = "Sales Update", message = "Sales are at 55%",
+#                                             icon = icon("bar-chart"), time = "22:00"),
+#                               messageItem(from = "Sales update", message = "Sales meeting at 6 PM",
+#                                            icon = icon("handshake-o"), time = "14:00")
+#                    )
                     ),
-    
-    
     dashboardSidebar(
       sliderInput("bins", "Número de barras", 1, 100, 50),
       sidebarMenu(
-      menuItem('Dashboard', tabName = 'dashbord', icon = icon('dashboard')),
+      menuItem('Dashboard', tabName = 'dashboard', icon = icon('dashboard')),
         menuSubItem('Dashboard Financeiro', tabName = "financeiro"),
         menuSubItem('Dashboard Vendas', 'vendas'),
       menuItem('Análises Detalhadas'),
@@ -29,14 +25,21 @@ shinyUI(
       tabItems(
         tabItem(tabName = "dashboard",
                 fluidRow(
-                  box(plotOutput("histogram")))),
-        tabItem(tabName = "financeiro", 
-                h1("Dashboard Financeiro")
-                ),
-        tabItem(tabName = "vendas", 
-                h2("Dashboard Vendas")
-        )
-      )
+                  box(
+                    plotOutput("histogram"))
+                  
+                  )),
+                
+              tabItem(tabName = "financeiro", 
+                    h1("Dashboard Financeiro")
+                    ),
+              
+            tabItem(tabName = "vendas", 
+                    h2("Dashboard Vendas")
+                )
+             
       )
     )
 )
+)
+
